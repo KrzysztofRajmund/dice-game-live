92710c7e0e606fab73bb93a620daf1fd1ec2f698
Krzysztof Nadlonek <knadlonek@gmail.com>
2021-07-13 21:44:52 +0200

    added player component boilerplate

--- dice-game/src/App.tsx
+++ dice-game/src/App.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 //components
-import RollDice from './components/RollDice'
+import RollDice from './components/RollDice';
+import Player from './components/Player';
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <div className="wrapper">
+        <Player />
         <RollDice />
+        <Player />
       </div>
     </Provider>
   );
---
+++ dice-game/src/components/Player.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Player: React.FC = () => {
+    return (
+        <section className='playerWrapper'>
+            <article>
+                <h1>Player 1</h1>
+                <h3>Bonus points:12</h3>
+            </article>
+            <article>
+                <h2>Total score: 99</h2>
+            </article>
+        </section>
+    )
+}
+
+export default Player;
